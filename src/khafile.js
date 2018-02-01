8450f032722e271a45d3e216a3f429223c88be93
Jeremy <cream@sourcre.am>
2018-01-31 23:43:12 -0800

    update demo

--- src/khafile.js
+++ src/khafile.js
@@ -3,7 +3,8 @@ let project = new Project('Perdita');
 project.addLibrary("jasper");
 project.addSources('Sources');
 project.addParameter('--connect 6000');
-project.addParameter('-debug');
+project.addParameter('-dce full');
+// project.addParameter('-debug');
 project.addAssets('Assets/**');
 
 resolve(project);
